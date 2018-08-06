b5a3ef87d899323219f791039ac6e67c8a0e8088
Madeline Stark <madeline.stark1@gmail.com>
2018-08-06 23:59:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,14 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT COUNT(species)
+  ";
+  "SELECT      `species`,
+               COUNT(`species`) AS `value_occurrence`
+      FROM     `characters`
+      GROUP BY `species`
+      ORDER BY `value_occurrence` DESC
+      LIMIT    1";
 end
 
 def select_name_and_series_subgenres_of_authors
