0b1f2e71880f21d1492e9905afaff54f082c4098
Madeline Stark <madeline.stark1@gmail.com>
2018-08-07 00:12:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,6 +41,7 @@ def select_series_title_with_most_human_characters
     FROM series
   INNER JOIN characters
       ON series.id = characters.series_id
+  WHERE characters.species = 'human'
   GROUP BY characters.species
   ORDER BY count(*) DESC
   LIMIT 1
