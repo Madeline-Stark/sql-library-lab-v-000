97d2f7c8a56ba72b27ec84bf332ca3eac135cae4
Madeline Stark <madeline.stark1@gmail.com>
2018-08-07 00:19:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,12 +42,16 @@ def select_series_title_with_most_human_characters
   INNER JOIN characters
       ON series.id = characters.series_id
   WHERE characters.species = 'human'
-  GROUP BY series.id 
+  GROUP BY series.id
   ORDER BY count(*) DESC
   LIMIT 1
   ";
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, count(character_books.book_id)
+    FROM characters
+  INNER JOIN character_books
+    ON characters.id = character_books.character_id
+  ";
 end
