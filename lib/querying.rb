70bb696fe958a9f8414c8ba4f1552f5370390ed2
Madeline Stark <madeline.stark1@gmail.com>
2018-08-07 00:10:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,12 +32,19 @@ def select_name_and_series_subgenres_of_authors
   INNER JOIN series
     ON authors.id = series.author_id
   INNER JOIN subgenres
-    ON series.subgenre_id = subgenres.id 
+    ON series.subgenre_id = subgenres.id
   ";
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+    FROM series
+  INNER JOIN characters
+      ON series.id = characters.series_id
+  GROUP BY characters.species
+  ORDER BY count(*) DESC
+  LIMIT 1
+  ";
 end
 
 def select_character_names_and_number_of_books_they_are_in
